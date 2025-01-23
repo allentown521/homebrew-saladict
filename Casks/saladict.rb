f9103ad14b4a7f3d3c1488c3088a5c8702e3cd28
allentown <69662387+allentown521@users.noreply.github.com>
2025-01-23 17:16:06 +0800

    Update saladict.rb (Saladict->saladict)

--- Casks/saladict.rb
+++ Casks/saladict.rb
@@ -5,7 +5,7 @@ cask "saladict" do
   sha256 arm:   "a71bca72ad2c38c4756bf667e578462cccdf2a74a66a715a7bb83aa4d13cf3c8",
          intel: "6a76d135a03576e019470e02656f0f8fa3d6a13706cc1d733736b7e66aa5eafe"
 
-  url "https://github.com/allentown521/saladict/releases/download/#{version}/Saladict_#{version}_#{arch}.dmg"
+  url "https://github.com/allentown521/saladict/releases/download/#{version}/saladict_#{version}_#{arch}.dmg"
   name "Saladict"
   desc "Cross-platform software for text translation and recognize"
   homepage "https://github.com/allentown521/saladict"
